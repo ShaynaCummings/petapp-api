e1b0f10d7a5538eb37dbe5f883780a63f602172a
shaynaCummings <shayna.cummings@gmail.com>
2014-12-12 00:57:49 -0500

    moved files; updated gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+PetApp::Application.config.secret_token = '2251ecd7af1dcb6540be763b481dbb9c733337c946ec85b80b21211a596e4ef8103249e6c18feabaa6621085e4e596e806bc1758d8e54737a784756c1154c824'
