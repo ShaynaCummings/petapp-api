8ac2b55588680b1581854380c82cf4eb26de4862
shaynaCummings <shayna.cummings@gmail.com>
2014-12-14 12:56:05 -0500

    added model and migration for supplies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212162628) do
+ActiveRecord::Schema.define(version: 20141214175417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,16 @@ ActiveRecord::Schema.define(version: 20141212162628) do
     t.integer  "veterinarian_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "supplies", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "vendor"
+    t.date     "purchase_date"
+    t.date     "paid_date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: true do |t|
