154c9dc15d6a20a45f408400de128cd3cfdf413c
shaynaCummings <shayna.cummings@gmail.com>
2014-12-18 13:31:12 -0500

    fixed rack cors issue (late yesterday)

--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,10 @@ module PetApp
   class Application < Rails::Application
 
 
-    config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
+    config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
 
